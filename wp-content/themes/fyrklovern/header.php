046a3c024eaac8fa605b086e90b7bf1310a8c626
Erik Dalgrund <erikdalgrund91@gmail.com>
2022-09-27 11:24:25 +0200

    Working on header.php
    Added the menu from the Wp-dashboard and icons for cart and search.

--- wp-content/themes/fyrklovern/header.php
+++ wp-content/themes/fyrklovern/header.php
@@ -4,6 +4,8 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>" />
   <meta name="viewport" content="width=device-width" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
   <?php wp_head(); ?>
 </head>
 <header class="headerpage">
