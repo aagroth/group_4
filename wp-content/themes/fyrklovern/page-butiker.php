05805a99dee4a23259b16348d498dfba07dc4700
Erik Dalgrund <erikdalgrund91@gmail.com>
2022-09-29 13:22:41 +0200

    Formated the code

--- wp-content/themes/fyrklovern/page-butiker.php
+++ wp-content/themes/fyrklovern/page-butiker.php
@@ -1,24 +1,26 @@
 <?php get_header() ?>
 <section class="page-store-section">
-<div class="title-content">
-    <h2 class="title-content__h2"> <?php the_content() ?> </h2>
-</div>
-<?php 
-	$stores = new WP_Query(array(
-		'posts_per_page' => 9,
-		'post_type' => 'wpll_store'
-	)); 
-   ?> <div class="store-grid"> <?php
-	while($stores -> have_posts()) {
-		$stores->the_post(); ?>	
+    <div class="title-content">
+        <h2 class="title-content__h2"> <?php the_content() ?> </h2>
+    </div>
+    <?php
+    $stores = new WP_Query(array(
+        'posts_per_page' => 9,
+        'post_type' => 'wpll_store'
+    ));
+    ?> <div class="store-grid"> 
+        <?php
+            while ($stores->have_posts()) { $stores->the_post(); 
+        ?>
             <div class="store-grid__child">
-			    <h2><?php the_title() ?></h2> 
-			    <p><?php the_content() ?></p>	
-            </div>	
-			<?php
-	}
-    ?> </div> 
- </section>
+                <h2><?php the_title() ?></h2>
+                <p><?php the_content() ?></p>
+            </div>
+        <?php
+        }
+        ?>
+    </div>
+</section>
 <?php
 ?>
 <?php get_footer() ?>
