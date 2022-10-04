514c9c8f6400208b8fd296a954289f849a6704e6
[adam] <92029070+aagroth@users.noreply.github.com>
2022-10-04 12:49:01 +0200

    Updated styles

--- wp-content/themes/fyrklovern/page-blocks/product-block-image-left.php
+++ wp-content/themes/fyrklovern/page-blocks/product-block-image-left.php
@@ -2,7 +2,7 @@
     <div class="product-block__image">
         <div class="product-block__container">
          <?php $image = get_field('image');
-            $size = 'medium'; 
+            $size = 'full'; 
         if( $image ) {
             echo wp_get_attachment_image( $image, $size );
         } ?>
@@ -11,7 +11,7 @@
     </div>
     <div class="product-block__info">
         <div>
-        <a> collection 1 </a>
+        <a> Knivar </a>
         <h1> <?php the_field('title') ?> </h1>
         <p> <?php the_field('price') ?> </p>
         <p> <?php the_field('description')?> </p>
